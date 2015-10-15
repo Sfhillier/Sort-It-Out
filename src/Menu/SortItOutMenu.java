ae19341c1a163acb203195ba226e616739b748fb
Scott Hillier <sfhillier@gmail.com>
2015-10-15 15:41:52 -0400

    Initial Commit

---
+++ src/Menu/SortItOutMenu.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Menu;
+
+/**
+ * @author Group 2
+ *
+ */
+public class SortItOutMenu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
