c90935bec088c5e301dc4eb5bbbc8d0998e9d54c
Roosevelt De leon <rooseveltd91@gmail.com>
2015-12-02 09:59:18 -0500

    completely started to do a bubble sort animation. this is not on our menu. its on the bubbleAnimation class. this approach could be better since we don't need everything on the paint component. I used the Timer from the util class to schedule the swapping nodes and succeeded in swapping the first nodes. yet, fail to come up with an algorithm to swap all the nodes.

--- src/Menu/bubbleAnimation.java
+++ src/Menu/bubbleAnimation.java
@@ -1,295 +1,170 @@
 package Menu;
-import java.applet.Applet;
+
+import java.awt.AlphaComposite;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.geom.Ellipse2D;
 
-public class bubbleAnimation extends Applet implements Runnable{
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-	// Background color for the panel
-	private static final Color BACKGROUND_COLOR = Color.black;
-	//Timer t = new Timer(1000,this);
+//import javax.swing.*;
+import java.util.*;
 
-	static int[] array = {5,6,7,2,1};
-	private static final int LENGTH = 5;
-	int d=0,m=0,z,t;
+@SuppressWarnings("serial")
+public class bubbleAnimation extends JPanel implements ActionListener {
 
-	int i;
-	String num = new String();
-	// indexes of each node.
-	// I think these should be constants to make it
-	// more easily understood that they won't change
-	int y = 0;
-	int width =50;
-	int height = 50;
+// Java.util timer which operates by schedules.
+    Timer t = new Timer();
 
-	int yString = 25;
+    int x=150;
+    int x1=150;
+    int x2=150;
+    int x3=150;
+    int x4=150;
+    
+    
+    int y=100;
+    int y1=100;
+    int y2=100;
+    int y3=100;
+    int y4=100;
 
-	//private SwingWorker<Void,Integer> worker;
+  //  int[] indexY = {y,y1,y2,y3,y4};
+   // int[] indexX = {x,x1,x2,x3,x4};
+   // int[] values = {5,1,4,2,8};
+    
+    public  bubbleAnimation(){
+	
+    	this.setBackground(Color.white); 
+    	
+    	//way to intantiate the animation. 
+    	t.scheduleAtFixedRate(down, 1000, 5);
+    	t.scheduleAtFixedRate(swap, 3000, 5);
+    	t.scheduleAtFixedRate(up, 5000, 5);
+ 
 
-	public void init()
-	{
+    }
+ 
+ 
 
-		setSize(800,800);
-		setBackground(BACKGROUND_COLOR);
-
+	public void paintComponent(Graphics g){
+		
+		super.paintComponent(g);
+	    Graphics2D g2 = (Graphics2D)g;
+	    
+	    // center the nodes.
+		g2.translate(50, 0);
+		
+		//create our nodes.
+		Shape ellipse = new Ellipse2D.Double(x-100, y, 50, 50);
+		Shape ellipse1 = new Ellipse2D.Double(x1-50, y1, 50, 50);
+		Shape ellipse2 = new Ellipse2D.Double(x2, y2, 50, 50);
+		Shape ellipse3 = new Ellipse2D.Double(x3+50, y3, 50, 50);
+		Shape ellipse4 = new Ellipse2D.Double(x4+100, y4, 50, 50);
+		
+		g2.setColor(Color.gray);
+		g2.fill(ellipse);
+		g2.fill(ellipse1);
+		g2.fill(ellipse2);
+		g2.fill(ellipse3);
+		g2.fill(ellipse4);
+		
+		// set transparency
+		AlphaComposite ac = AlphaComposite.getInstance(AlphaComposite.DST_ATOP, 0.4f);
+		g2.setComposite(ac);
+		
+		// draw transparent text
+		Font font = new Font("Serif", Font.BOLD, 30);
+		g2.setFont(font);
+		g2.drawString("5", x-83, y+35);
+		g2.drawString("1", x1-33, y1+35);
+		g2.drawString("4", x2+17, y2+35);
+		g2.drawString("2", x3+67, y3+35);
+		g2.drawString("8", x4+117, y4+35);
+		
+	    
+	    repaint();
+	    
+	    
 	}
-	public void start(){
-
-		Thread t = new Thread();
-		t.start();
-	}
-
-	public void run() {
-		// TODO Auto-generated method stub
-
-
-	}
-
-	//where all the magic happens.
-
-	public void paint(Graphics g){
-
-		// super.paintComponents(g);
-
-		//change the origin to the center.
-		g.translate(this.getWidth()/2, this.getHeight()/2);
-
-		//this will render our nodes.
-		for(i=0; i<LENGTH; i++){
-			g.setColor(Color.white);
-			num = Integer.toString(array[i]);
-			g.drawRect(-150+i*50, 0 , 30 , 30);
-			g.drawString(num,-138 + i*50 , 20);	
-		}	
-		try
-		{
-			Thread.sleep(30);
-		}
-		catch(Exception e){}	
-
-		//bubble sort algorithm
-		int j,temp=0;	
-		for(i=0; i<LENGTH;i++)
-			for( j=i; j<LENGTH-1; j++)
-				if(array[i] > array[j+1]){
-					//the start of our animations since we start updating some variables.		
-
-					int a=i,b=j+1,count;		
-					for( count =0; count<50;count++){
-
-						//black background so lets wear white. this will draw a node on top of the one we have.
-						g.setColor(Color.white);
-						num = Integer.toString(array[a]);
-						g.drawRect(-150+a*50,0+count*4,30,30);
-						g.drawString(num,-138+a*50,20+count*4);
-
-						try
-						{
-							Thread.sleep(15);
-						}
-						catch(Exception e){}
-						// creates and moves our node down
-						g.setColor(Color.black);
-						num = Integer.toString(array[a]);
-						g.drawRect(-150 + a*50, 0 + count*4 , 30, 30);
-						g.drawString(num, -138 + a*50, 20 + count*4);
-					}
-					//back to origin.
-					g.setColor(Color.white);
-					num = Integer.toString(array[a]);
-					g.drawRect(-150+a*50,0+count*4,30,30);
-					g.drawString(num,-138+a*50,20+count*4);
-					for(count=0;count<50;count++)
-					{
-						//moves down the other node 
-						g.setColor(Color.white);
-						num = Integer.toString(array[b]);
-						g.drawRect(-150+b*50,0+count*4,30,30);
-						g.drawString(num,-138+b*50,20+count*4);
-
-						try
-						{
-							Thread.sleep(10);
-						}
-						catch(Exception e){}
-
-						//these are not visible 
-						g.setColor(Color.black);
-						num = Integer.toString(array[b]);
-						g.drawRect(-150+b*50,0+count*4,30,30);
-						g.drawString(num,-138+b*50,20+count*4);
-					}
-					num = Integer.toString(array[b]);
-					g.drawRect(-150+b*50,0+count*4,30,30);
-					g.drawString(num,-138+b*50,20+count*4);
-
-					//now time to mix the code
-
-					for(int z=0;z<(b-a)*50;z++){
-
-						g.setColor(Color.white);
-						//causes the bottom to swap
-
-						num = Integer.toString(array[a]);
-						g.drawRect(-150+a*50+z,200,30,30);
-						g.drawString(num,-138+a*50+z,220);
-
-						g.setColor(Color.white);
-
-						num = Integer.toString(array[a]);
-						g.drawRect(-150+b*50-z,200,30,30);
-						g.drawString(num,-138+b*50-z,220);
-
-						try
-						{
-							Thread.sleep(20);
-						}
-						catch(Exception e){}
-
-						g.setColor(Color.black);
-						num = Integer.toString(array[a]);
-						g.drawRect(-150+a*50+z,200,30,30);
-						g.drawString(num,-138+b*50+z,220);
-
-						g.setColor(Color.black);
-						num = Integer.toString(array[b]);
-						g.drawRect(-150+b*50-z,200,30,30);
-						g.drawString(num,-138+b*50-z,220);
-					}
-					g.setColor(Color.white);
-					num = Integer.toString(array[a]);
-					g.drawRect(-150+a*50+z,200,30,30);
-					g.drawString(num,-138+a*50+z,220);
-
-					g.setColor(Color.white);
-					num = Integer.toString(array[b]);
-					g.drawRect(-150+b*50-z,200,30,30);
-					g.drawString(num,-138+b*50-z,220);
-
-					for(int t=50; t>0;t--)
-					{
-
-						g.setColor(Color.black);				
-						num = Integer.toString(array[b]);
-						g.drawRect(-150+a*50,0+t*4,30,30);
-						g.drawString(num,-138+a*50,20+t*4);
-
-						try
-						{
-							Thread.sleep(10);
-						}
-						catch(Exception e)
-						{}
-
-						g.setColor(Color.black);				
-						num = Integer.toString(array[b]);
-						g.drawRect(-150+a*50,320+t*4,30,30);
-						g.drawString(num,-138+a*50,20+t*4);
-
-					}
-
-					g.setColor(Color.white);				
-					num = Integer.toString(array[b]);
-					g.drawRect(-150+a*50,0+t*4,30,30);
-					g.drawString(num,-138+a*50,20+t*4);
-
-					for(t=50;t>0;t--)
-					{
-						g.setColor(Color.black);				
-						num = Integer.toString(array[a]);
-						g.drawRect(-150+b*50,0+t*4,30,30);
-						g.drawString(num,-138+b*50,20+t*4);
-						try
-						{
-							Thread.sleep(15);
-						}
-						catch(Exception e){}
-
-						g.setColor(Color.black);				
-						num = Integer.toString(array[a]);
-						g.drawRect(-150+b*50,320+t*4,30,30);
-						g.drawString(num,-138+b*50,20+t*4);
-					}
-
-					g.setColor(Color.white);				
-					num = Integer.toString(array[a]);
-					g.drawRect(-150+b*50,0+t*4,30,30);
-					g.drawString(num,-138+b*50,20+t*4);
-					temp = array[i];
-					array[i]= array[j+1];
-					array[j+1]= temp;
-
-
-
-				}
-	}
-
-
-
-
-	/*
-	public static void main(String[] args) {
-
-
-
-		bubbleAnimation sort = new bubbleAnimation();
-		JFrame frame = new JFrame();
-		frame.add(sort);
-		frame.setVisible(true);
-		frame.setSize(600,400);
-		frame.setBackground(BACKGROUND_COLOR);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLocationRelativeTo(null);
-	}
-
-
-
-
-	/*ActionListener taskPerformer = new ActionListener() {
-
-	public void actionPerformed(ActionEvent e) {
-
-		worker = new SwingWorker<Void, Integer>(){
-
-			@Override
-			protected Void doInBackground() throws Exception {
-				// TODO Auto-generated method stub
-				for(int i =0; i<=5; i++){
-
-				y = y-10;
-			yString = yString - 10;	
-
-
-		}
-
-				return null;
-			}};
-
-			worker.execute();
-
-
-	}
-	};
+	
+	/*******************************
+	 * start of first swap
 	 */
+	
+	TimerTask down = new TimerTask(){
+		public void run(){
+			
+			y=y+1;
+			y1=y1+1;
+			if(y==300){
+				
+				y=y-1;
+				y1=y1-1;
+			}
+		}
+	};
+	
+	TimerTask swap = new TimerTask(){
 
+		//@Override
+		public void run(){
+			
+			x = x+1;
+			x1 = x1-1;
+			if(x1==100){
+				x = x-1;
+				x1 = x1+1;	
+			}
+		}
+	};
+	
+	TimerTask up = new TimerTask(){
 
+		public void run(){
+			
+			y = y-2;
+			y1 = y1-2;
+			if(y<100){
+				y = y+1;
+				y1 = y1+1;	
+			}
+		}
+	};
+	
+	
+	/*******************************
+	 * end of first  swap
+	 */
+	
+	
+	
+	public void actionPerformed(ActionEvent e){
+		
+		
+			
+			
+	}		
 
+    
 
-
-
-
-
-
+	
+	
+	public static void main(String[] args) {
+		
+		bubbleAnimation s = new bubbleAnimation();
+		JFrame f = new JFrame();
+		f.add(s);
+		f.setVisible(true);
+		f.setSize(450,600);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setLocationRelativeTo(null);
+	}
 
 }
-
-
-
-
-
-
-
-
