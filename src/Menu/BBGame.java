a616bcdeca542f75553e3719b6ecb8a83c42dd05
bridget <bridget@bridget-pc>
2015-12-05 12:02:40 -0500

    i got this to work only if the sort is done completely, im stil working on making sure the random numbers do not repeat and so the text field only accept ints

--- src/Menu/BBGame.java
+++ src/Menu/BBGame.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,156 +16,170 @@ import java.util.Random;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class BBGame extends JPanel implements ActionListener {
+  
+   //declare ints
+	    Random rand = new Random();
+		 int[] rNum = new int[5];
+		 int[] arr = new int[5];
+		    //private static final String Empty = "Empty";
+		    JLabel RandomNum;
+		    JLabel bubbleSort;
+		    JTextField firstNum;
+		    JTextField secondNum;
+		    JTextField thirdNum;
+		    JTextField fourthNum;
+		    JTextField fifthNum;
+		    JLabel sortSign ;
+		    JLabel RandomNum1;
+		    JLabel RandomNum2;
+		    JLabel RandomNum3;
+		    JLabel RandomNum4; 
+		    JLabel RandomNum5; 
+		    JTextArea answer;
+		static JLabel space = new JLabel("                    " );
+	    private final JLabel sortedNum = new JLabel("sorted : ");
+	    private final JTextArea result = new JTextArea(" ");
+		private String sansarif;
+	    private static final Dimension TEXT_INPUT_DIM = new Dimension(50, 30);
+	   // private static final Dimension TEXT_OUTPUT_DIM = new Dimension(20, 50);
 
-	//declare ints
-	Random rand = new Random();
-	int[] rNum = new int[5];
-	int[] arr = new int[5];
-	//private static final String Empty = "Empty";
-	JLabel RandomNum;
-	JTextField firstNum;
-	JTextField secondNum;
-	JTextField thirdNum;
-	JTextField fourthNum;
-	JTextField fifthNum;
-	JLabel sortSign ;
-	JLabel RandomNum1;
-	JLabel RandomNum2;
-	JLabel RandomNum3;
-	JLabel RandomNum4; 
-	JLabel RandomNum5; 
-	JTextArea answer;
-	static JLabel space = new JLabel("                    " );
-	private final JLabel sortedNum = new JLabel("sorted : ");
-	private final JTextArea result = new JTextArea(" ");
-	private static final Dimension TEXT_INPUT_DIM = new Dimension(50, 30);
-	// private static final Dimension TEXT_OUTPUT_DIM = new Dimension(20, 50);
+	    
+    public BBGame() {
+    	for(int i = 0;i< rNum.length;++i){
+    		
+    	rNum[i]= rand.nextInt(100);
+       	//rNum[1]= rand.nextInt(100);
+       //	rNum[2]= rand.nextInt(100);
+       	//rNum[3]= rand.nextInt(100);
+       //	rNum[4]= rand.nextInt(100);
+    		}
 
+    	//add items to gui
+        setLayout(new GridLayout(4, 1));
+        JPanel game = new JPanel();
+        game.setLayout(new FlowLayout(10));
+        sortSign = new JLabel("Sort these numbers: ");
+        
+        RandomNum1 = new JLabel(Integer.toString(rNum[0]));
+	    RandomNum2= new JLabel(Integer.toString(rNum[1]));
+	    RandomNum3= new JLabel(Integer.toString(rNum[2]));
+	    RandomNum4= new JLabel(Integer.toString(rNum[3])); 
+	    RandomNum5= new JLabel(Integer.toString(rNum[4])); 
+        firstNum = new JTextField();
+        firstNum.setPreferredSize(TEXT_INPUT_DIM);
+        secondNum = new JTextField("");
+        secondNum.setPreferredSize(TEXT_INPUT_DIM);
+        thirdNum = new JTextField("");
+        thirdNum.setPreferredSize(TEXT_INPUT_DIM);
+        fourthNum = new JTextField("");
+        fourthNum.setPreferredSize(TEXT_INPUT_DIM);
+        fifthNum = new JTextField("");
+        fifthNum.setPreferredSize(TEXT_INPUT_DIM);
+       bubbleSort = new JLabel("Bubble Sort");
+       bubbleSort.setFont(bubbleSort.getFont().deriveFont(24f));
+       // game.add(RandomNum);
+        game.add(firstNum);
+        game.add(secondNum);
+        game.add(thirdNum);
+        game.add(fourthNum);
+        game.add(fifthNum);
+        game.add(SortButton());
+        add(title());
+        add(initNum());
+        add(game);
+        add(createPanelResult());
+    }
 
-	public BBGame() {
-		rNum[0]= rand.nextInt(100);
-		rNum[1]= rand.nextInt(100);
-		rNum[2]= rand.nextInt(100);
-		rNum[3]= rand.nextInt(100);
-		rNum[4]= rand.nextInt(100);
+    private Component SortButton() {
+        JButton sortButton = new JButton("Sort it out!");
+        sortButton.addActionListener(this);
+        return sortButton;
+    }
+private final JPanel title(){
+	 JPanel t = new JPanel();
+     t.setLayout(new FlowLayout());
+	 t.add(bubbleSort);
+	 return t;
+}
+    private final JPanel createPanelResult() {
+        JPanel p = new JPanel();
+        p.setLayout(new BorderLayout());
+        p.add(sortedNum, BorderLayout.WEST);
+        p.add(result, BorderLayout.CENTER);
+       // p.add(answer,BorderLayout.SOUTH);
+        return p;
+    }
 
-		//add items to gui
-		setLayout(new GridLayout(3, 1));
-		JPanel game = new JPanel();
-		game.setLayout(new FlowLayout(10));
-		sortSign = new JLabel("Sort these numbers: ");
-		RandomNum1 = new JLabel(Integer.toString(rNum[0]));
-		RandomNum2= new JLabel(Integer.toString(rNum[1]));
-		RandomNum3= new JLabel(Integer.toString(rNum[2]));
-		RandomNum4= new JLabel(Integer.toString(rNum[3])); 
-		RandomNum5= new JLabel(Integer.toString(rNum[4])); 
-		firstNum = new JTextField("");
-		firstNum.setPreferredSize(TEXT_INPUT_DIM);
-		secondNum = new JTextField("");
-		secondNum.setPreferredSize(TEXT_INPUT_DIM);
-		thirdNum = new JTextField("");
-		thirdNum.setPreferredSize(TEXT_INPUT_DIM);
-		fourthNum = new JTextField("");
-		fourthNum.setPreferredSize(TEXT_INPUT_DIM);
-		fifthNum = new JTextField("");
-		fifthNum.setPreferredSize(TEXT_INPUT_DIM);
-
-		// game.add(RandomNum);
-		game.add(firstNum);
-		game.add(secondNum);
-		game.add(thirdNum);
-		game.add(fourthNum);
-		game.add(fifthNum);
-		game.add(SortButton());
-		add(initNum());
-		add(game);
-		add(createPanelResult());
-	}
-
-	private Component SortButton() {
-		JButton sortButton = new JButton("Sort it out!");
-		sortButton.addActionListener(this);
-		return sortButton;
-	}
-
-	private final JPanel createPanelResult() {
-		JPanel p = new JPanel();
-		p.setLayout(new BorderLayout());
-		p.add(sortedNum, BorderLayout.WEST);
-		p.add(result, BorderLayout.CENTER);
-		// p.add(answer,BorderLayout.SOUTH);
-		return p;
-	}
-
-	private final JPanel initNum() {
-		JPanel r = new JPanel();
-		r.setLayout(new FlowLayout(10));
-		r.add(sortSign);
-		r.add(RandomNum1);
-		r.add(RandomNum2);
-		r.add(RandomNum3);
-		r.add(RandomNum4);
-		r.add(RandomNum5);
-
-		return r;
-	}
-
-	public static void main(String[] args) {
-		JFrame f = new JFrame();
-		f.setSize(500, 300);
-		f.setLocationRelativeTo(null);
-		f.add(new BBGame());
-		f.setVisible(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		inputNum();
-		bubbleSort();
-		//isEqual();
-		resultLabel();
-	}  
-
-	public void bubbleSort(){
-		int temp;
-		for(int i=0; i< rNum.length-1;i++){
-			for(int j =1; j < rNum.length-i;j++ ){
-				if(rNum[j-1]>rNum[j]){
-					temp = rNum[j-1];
-					rNum[j-1] = rNum[j];
-					rNum[j]=temp;}			
-			}System.out.print( Arrays.toString(rNum));
-		}
-	}
-
-	public void inputNum(){
-		arr[0]= Integer.parseInt(firstNum.getText());
-		arr[1]= Integer.parseInt(firstNum.getText());
-		arr[2]= Integer.parseInt(firstNum.getText());
-		arr[3]= Integer.parseInt(firstNum.getText());
-		arr[4]= Integer.parseInt(firstNum.getText());
-	}
-
-	/*  public boolean isEqual(int[] array){
+    private final JPanel initNum() {
+        JPanel r = new JPanel();
+        r.setLayout(new FlowLayout(10));
+        r.add(sortSign);
+        r.add(RandomNum1);
+        r.add(RandomNum2);
+        r.add(RandomNum3);
+   	 	r.add(RandomNum4);
+   	 	r.add(RandomNum5);
+        return r;
+    }
+ 
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        f.setSize(600, 400);
+        f.setLocationRelativeTo(null);
+        f.add(new BBGame());
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+  
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    	bubbleSort();
+    	inputNum();
+    	//isEqual();
+    	resultLabel();
+            }  
+    
+    public void bubbleSort(){
+    	int temp;
+    	for(int i=0; i< rNum.length-1;i++){
+    		for(int j =1; j < rNum.length-1;j++ ){
+    			if(rNum[j-1]>rNum[j]){
+    			temp = rNum[j-1];
+    			rNum[j-1] = rNum[j];
+    			rNum[j]=temp;}			
+    		}System.out.print( Arrays.toString(rNum));
+    	}return ;
+    }
+    
+     public void inputNum(){
+    
+    	arr[0]= Integer.parseInt(firstNum.getText());
+    	arr[1]= Integer.parseInt(secondNum.getText());
+    	arr[2]= Integer.parseInt(thirdNum.getText());
+    	arr[3]= Integer.parseInt(fourthNum.getText());
+    	arr[4]= Integer.parseInt(fifthNum.getText());
+    	    }
+     
+  /*  public boolean isEqual(int[] array){
     if(this.arr.length != rNum.length){return false;}
       for(int index =0; index<arr.length; index++){
     	  if(this.arr[index]!= rNum[index]){return false;}
       }return true;
     }*/
-
-	private void resultLabel() {
-		/* answer.setText("");
+    
+    private void resultLabel() {
+      /* answer.setText("");
        for (int i =0; i< rNum.length;i++){
 			   answer.append(rNum[i]+"\n");
             } 
     }*/
-		for(int i = 0; i < arr.length; i++){
+    	for(int i = 0; i < arr.length; i++){
 
 			if (arr[i] == rNum[i]){
 				result.append(rNum[i]+"\n");
@@ -173,4 +188,5 @@ public class BBGame extends JPanel implements ActionListener {
 				result.append("Try Again ");	
 		}
 	}
-}
+    }
+
